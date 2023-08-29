a76c78c812e8ede1f9bcdd6a7197c08f02f5938d
Dmitry177777 <k1791@mail.ru>
2023-08-29 22:57:03 +0500

    step4 фильтрация

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 
 from rest_framework import viewsets, generics, routers
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django.urls import path, include
 from main.models import Well, Lesson, Payment
 from rest_framework.response import Response
@@ -10,6 +11,9 @@ from django.shortcuts import get_object_or_404
 class PaymentViewSet(viewsets.ModelViewSet):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['well_name', 'payment_method']
+    ordering_fields = ['date_of_payment']
 
 class WellViewSet(viewsets.ModelViewSet):
     serializer_class = WellSerializer
