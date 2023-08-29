dea9f80f7dd40ef20a7d8485a284f7957ad4b14a
Dmitry177777 <k1791@mail.ru>
2023-08-29 22:39:19 +0500

    step3 fill

--- main/urls.py
+++ main/urls.py
@@ -4,14 +4,14 @@ from main import views
 from main.apps import MainConfig
 from rest_framework.routers import DefaultRouter
 
-from main.views import (WellViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView,
-                        LessonDestroyAPIView)
+from main.views import (WellViewSet, PaymentViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView,
+                        LessonUpdateAPIView, LessonDestroyAPIView)
 
 app_name = MainConfig.name
 
 router = DefaultRouter()
 router.register(r'well', WellViewSet, basename='well')
-
+router.register(r'payment', PaymentViewSet, basename='payment')
 
 urlpatterns = [path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
                path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
